4a5b2f635b549f980e8362751664a5404b4fada6
WeakerGravity22 <lalithveerendrach@gmail.com>
2025-11-19 21:58:03 -0800

    internal commit

---
+++ MySwap.java
@@ -0,0 +1,19 @@
+public class MySwap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Swapping using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}                                                          
